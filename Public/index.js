5b8dd001f4a81de9855dcf26de7d6389159b30dd
DylanDev92 <changliangdylan@gmail.com>
2022-09-21 21:31:02 -0400

    Finished

--- Public/index.js
+++ Public/index.js
@@ -1,4 +1,28 @@
+let validUrl = false;
+
 async function onClickButton(){
     let url = document.getElementById('input-url-send').value;
-    await fetch('/send', { method: "POST", headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({URL: url})})
+    fetch('/send', { method: "POST", headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({URL: url})}).then(res => {
+        if (res.status == 400){
+            document.getElementById('url-shorted').textContent = "Not valid URL"
+            validUrl = false;
+            return;
+        }
+        res.json().then(json => {
+            document.getElementById('url-shorted').textContent = json.URL;
+            validUrl = true;
+        })
+    })
+}
+
+function copyToClipboard(){
+    if (validUrl == false) return;
+    navigator.clipboard.writeText(document.getElementById('url-shorted').textContent);
+
+    let modal = document.getElementById("modalid");
+    modal.style.display = "block";
+
+    setTimeout(() => {
+        modal.style.display = "none";
+    }, 1000)
 }
--- Server/server.js
+++ Server/server.js
@@ -8,8 +8,57 @@ app.use(express.json({limit: "1mb"}));
 app.use(express.static("../Public"));
 
 app.post('/send', (req, res) => {
-    console.log(req.body);
-    if (req.body.URL.toString().startsWith(".")){
-        console.log(req.body);
+    let url = req.body.URL.toString();
+    if (isValidHttpUrl(url)){
+        console.log("Passed");
+        let idAdd = generateUID(6);
+        while (true){
+            if (shortedUrlList.some(x => x.URL == url)){
+                idAdd = shortedUrlList.find(x => x.URL == url).ID;
+                break;
+            }
+            if (!shortedUrlList.some(x => x.ID == idAdd)){
+                shortedUrlList.push({'ID': idAdd, 'URL': url})
+                break;
+            }
+            idAdd = generateUID(6);
+        }
+        console.log(shortedUrlList);
+        res.status(200).json({"URL": process.env.URL + idAdd})
+    }
+    else {
+        console.log("NoPassed");
+        res.status(400).send()
     }
 })
+
+app.get('/:id', (req, res) => {
+    let idUrl = req.params.id;
+
+    try {
+        res.redirect(shortedUrlList.find(x => x.ID == idUrl).URL)
+    }
+    catch {
+        res.send("Not valid link.")
+    }
+})
+
+var shortedUrlList = [];
+
+function generateUID(length)
+{
+    return Array(length).fill(0).map(x => Math.random().toString(36).charAt(2)).join('');
+}
+
+function isValidHttpUrl(string) {
+    let url;
+    
+    try {
+      url = new URL(string);
+      console.log("hello")
+    } catch {
+      return false;  
+    }
+  
+    return url.protocol === "http:" || url.protocol === "https:";
+}
